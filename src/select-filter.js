093773d4e7611678f00a6d66500fb8639e7205e0
vkyin <86160576@qq.com>
2016-05-12 19:33:03 +0800

    init code again

--- src/select-filter.js
+++ src/select-filter.js
@@ -2,55 +2,154 @@
     function SelectFilter(dom, data, options) {
         var _self = this,
             defaults = {
-                filterFn: function ()
+                listBuilder: function (data) {
+                    return data;
+                },
+                ajaxUrl: '',
+                ajaxMethod: 'get',
+                ajaxData: null,
+                ajaxSuccess: function () {},
+                ajaxError: function () {},
+                filterKey: undefined
             };
         if (!isArray(data) && isObject(data)) {
             options = data;
             this.data = options.data;
         } else {
-            options = {};
             options.data = this.data = data;
         }
         this.dom = dom;
         this.options = extend(defaults, options);
+        this.container = document.createElement('div');
         this.ul = document.createElement('ul');
+        this.lastLi = document.createElement('li');
         this.domOldValue;
 
         this.updateList();
-        this._bindEvent();
+        _initLastLi.call(this);
+        _bindEvent.call(this);
+        _initDom.call(this);
     }
-
-    SelectFilter.prototype._bindEvent = function () {
-        var _self = this;
-        _self.dom
-        bind(_self.dom, 'keyup', function () {
-            _self.updateList();
-        });
-        bind(_self.dom, 'keydown', function () {
-            _self.domOldValue = _self.dom.value;
-        });
-    };
-
 
     SelectFilter.prototype.updateList = function () {
         var _self = this,
+            data, temp, listBuilder = _self.options.listBuilder,
+            html = [];
+        data = _filter.call(_self);
+        data.forEach(function (e) {
+            html.push('<li>' + listBuilder.call(_self, e) + '</li>');
+        });
+        _self.ul.innerHTML = html.join('');
+        _self.ul.appendChild(_self.lastLi);
+    }
+
+    window.SelectFilter = SelectFilter;
+
+
+    //内部函数
+    function _filter() {
+        var _self = this,
             key = _self.dom.value,
-            data, filterFn = _self.options.filterFn;
-        if (key === _self.domOldValue) {
-            return;
-        }
+            i, l,
+            filterKey = _self.options.filterKey,
+            data;
+
         console.log(key);
+        //过滤逻辑
+        //从_self.data中根据key筛选数据，并push到data数组中
         if (key === '') {
             data = _self.data.slice();
         } else {
             data = [];
-            _self.data.forEach(function (e) {
-
-            });
+            if (isUndefined(filterKey)) {
+                _self.data.forEach(function (e) {
+                    if (e.indexOf(key) > -1) {
+                        data.push(e);
+                    }
+                });
+            } else if (isArray(filterKey)) {
+                _self.data.forEach(function (e) {
+                    for (i = 0, l = filterKey.length; i < l; i++) {
+                        if (e.hasOwnProperty(filterKey[i]) && e[filterKey[i]].indexOf(key) > -1) {
+                            data.push(e);
+                            break;
+                        }
+                    }
+                });
+            } else if (isString(filterKey)) {
+                _self.data.forEach(function (e) {
+                    if (e.hasOwnProperty(filterKey) && e[filterKey].indexOf(key) > -1) {
+                        data.push(e);
+                    }
+                });
+            } else {
+                console.error('filterKey must be an Array or a String or undefined')
+            }
         }
-    };
-    window.SelectFilter = SelectFilter;
+        console.log(data);
+        //过滤逻辑end
+        return data;
+    }
 
+    function _initLastLi() {
+        var _self = this,
+            input, btn, options = _self.options;
+        input = document.createElement('input');
+        btn = document.createElement('button');
+        btn.innerHTML = '提交';
+        bind(btn, 'click', function () {
+            btn.disabled = 'disabled';
+            ajax({
+                url: options.ajaxUrl,
+                method: options.ajaxMethod,
+                data: options.ajaxData,
+                success: function (data) {
+                    var temp = options.ajaxSuccess(data, input.value);
+                    if (!isUndefined(temp)) {
+                        _self.data.push(temp);
+                        _self.updateList();
+                    }
+                    btn.disabled = '';
+                },
+                error: function (d) {
+                    options.ajaxError(d);
+                }
+            });
+        });
+        _self.lastLi.appendChild(input);
+        _self.lastLi.appendChild(btn);
+    }
+
+    function _bindEvent() {
+        var _self = this;
+        bind(_self.dom, 'keyup', function () {
+            if (_self.dom.value === _self.domOldValue) {
+                return;
+            }
+            _self.domOldValue = _self.dom.value;
+            _self.updateList();
+        });
+        bind(_self.dom, 'focus', function (e) {
+            _self.ul.style.display = 'block';
+        });
+        bind(document, 'click', function (e) {
+            var i, l, el;
+            for (i = 0, l = e.path.length; i < l; i++) {
+                el = e.path[i];
+                if (el === _self.dom || el === _self.ul) {
+                    return;
+                }
+            }
+            _self.ul.style.display = 'none';
+        });
+    }
+
+    function _initDom() {
+        this.ul.style.display = 'none';
+        this.dom.parentNode.insertBefore(this.container, this.dom);
+        this.container.appendChild(this.dom);
+        this.container.appendChild(this.ul);
+    }
     //通用函数
     var bind = (function () {
         if (window.addEventListener) {
@@ -64,6 +163,56 @@
         }
     })();
 
+    function ajax(options) {
+        var xhr = new XMLHttpRequest(),
+            defaults = {
+                method: 'get',
+                url: '',
+                data: null,
+                contentType: 'application/x-www-form-urlencoded',
+                success: function () {},
+                error: function () {}
+            };
+        extend(defaults, options);
+        if (xhr) {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    console.log(xhr)
+                    if (xhr.status == 200) {
+                        options.success.call(xhr, xhr.responseText)
+                    } else {
+                        options.error.call(xhr, xhr.responseText)
+                    }
+                }
+            }
+            if (defaults.method.toLowerCase() === 'get') {
+                xhr.open('get', defaults.url + '?' + serialize(defaults.data), true);
+                xhr.send(null);
+            } else {
+                xhr.open('post', defaults.url, true);
+                xhr.send(serialize(defaults.data));
+            }
+        }
+    }
+
+    function serialize(param) {
+        var suffix = '',
+            key;
+        if (isObject(param)) {
+            for (key in param) {
+                if (suffix === '') {
+                    suffix += encodeURIComponent(key) + '=' + encodeURIComponent(param[key]);
+                } else {
+                    suffix += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(param[key]);
+                }
+            }
+            return suffix;
+        } else if (isString(param)) {
+            return param;
+        }
+
+    }
+
     function extend(target, src) {
         var x;
         for (x in src) {
@@ -76,8 +225,16 @@
         return Object.prototype.toString.call(o) === '[object Array]';
     }
 
+    function isString(o) {
+        return Object.prototype.toString.call(o) === '[object String]';
+    }
+
     function isObject(o) {
         return Object.prototype.toString.call(o) === '[object Object]';
+    }
+
+    function isUndefined(o) {
+        return Object.prototype.toString.call(o) === '[object Undefined]';
     }
 
 })(window);
